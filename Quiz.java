2a03c17c3d40fdc1205c80da6e5f376fd7851539
smiave20 <avery.smith@stu.daviess.kyschools.us>
2026-02-09 20:31:41 +0000

    FRQ-Quiz

--- Quiz.java
+++ Quiz.java
@@ -1,10 +1,27 @@
 public static String scrambleWord(String word){
   /* To be implemented in part (a) */
+    String equalsA = "A";
+    String notA = word;
 
+  for(int i = 0; i < word.length()-1; i++)
+    
+    if(i.equals("A") && (!(i.equals("A")+1)))
+      return notA + equalsA;
+    else
+      return word;
+  
+    
 }
 
 
 public static void scrambleOrRemove(List<String> wordList){
   /* To be implemented in part (b) */
 
+  for(int i = wordList.length()-1; i >= 0; i--)
+    if(wordList.get(i).equals(scrambleWord()))
+      wordList.remove(i);
+    else
+      wordList.scrambleWord(i);
+
+    return wordList;
 }
